c9c2714bd50e5ddfb040a501c21e82a196cbb141
declancockburn <apexaviour@gmail.com>
2020-10-17 23:47:53 +0200

    minor: more unittest trying

--- unit_testing/main.py
+++ unit_testing/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import numpy as np
+import pytest
 
 
-# Press the green button in the gutter to run the script.
+def return_greeting(name):
+    return f'Hi, {name}'
+
+def split_array_in_two(a):
+    if not isinstance(a, np.ndarray):
+        raise ValueError
+    halfway = len(a) // 2
+    return np.array([a[:halfway], a[halfway:]])
+
+
+# split_array_in_two(np.array([1,2,3,4,5,6]))
+# split_array_in_two("np.array([1,2,3,4,5,6])")
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    exit_code = pytest.main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return_greeting('PyCharm')
+
+
---
+++ unit_testing/test_first_testing.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+from main import return_greeting, split_array_in_two
+
+
+def test_print_hi():
+    assert return_greeting("Declan") == "Hi, Declan"
+
+
+def test_split_array_works():
+    example_data = np.array([1,2,3,4,5,6])
+    actual = split_array_in_two(example_data)
+    expected = np.array([[1,2,3],
+                        [4,5,6]])
+    assert actual == pytest.approx(expected), f"Expected {expected}, got {actual}"
+
+    # example_data_str = str(example_data)
+    # with pytest.raises(ValueError):
+    #     split_array_in_two(example_data_str)
+
+# test_split_array_works()
